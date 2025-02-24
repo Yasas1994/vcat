c8c52ac4405f65d4b43e1cb3b5aec9620ad8d75c
Yasas1994 <yasas.wijesekara@gmail.com>
2025-02-24 10:24:44 +0100

    script to remove empty fasta records

---
+++ scripts/remove_empty_records.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from Bio import SeqIO
+from sys import argv
+from pathlib import Path
+path = Path(argv[1])
+seq_records = []
+for current_seq in SeqIO.parse(path, "fasta"):
+    if len(current_seq.seq) != 0:
+        seq_records.append(current_seq)
+
+# Write the genome sequences to a file
+
+new_path = path.parent / (path.name + '.tmp')
+
+with open(new_path, "w") as output_handle:
+    SeqIO.write(seq_records, output_handle, "fasta")
